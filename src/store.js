19d1b6966a19964e7c226b15fc48b53b20e559f2
rodrigcasio <rod.casio@outlook.com>
2025-09-09 15:17:46 -0600

    Imported "mealsSlice.js" into the redux store to import details

--- src/store.js
+++ src/store.js
@@ -2,11 +2,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import venueReducer from './venueSlice';
 import avReducer from './avSlice.js';
-// 3. adding the into the store the avSlice
+import mealsReducer from './mealsSlice.js';
+// 3. adding into the redux store the avSlice
+// 3.2 adding into the redux store the mealsSlice
 
 export default configureStore({
   reducer: {
     venue: venueReducer,    
     av: avReducer,    // same name as the key-value pair 'name: "av" in avSlice.js
+    meals: mealsReducer,
   },
 });
