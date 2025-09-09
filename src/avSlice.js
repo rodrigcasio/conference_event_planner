60db076cbcee53ef3126f1f94c48e3a6339373fa
rodrigcasio <rod.casio@outlook.com>
2025-09-09 13:25:32 -0600

    Added objects inside the array varible "initialState"

--- src/avSlice.js
+++ src/avSlice.js
@@ -1,9 +1,40 @@
 import { createSlice } from "@reduxjs/toolkit";
+// 1. adding objects to provide data structure
 
 export const avSlice = createSlice({
   name: "av",
   initialState: [
-    
+    {
+      img: "",
+      name: "Projectors",
+      cost: 200,
+      quantity: 0,
+    },
+    {
+      img: "",
+      name: "Speaker",
+      cost: 35,
+      quantity: 0,
+    },
+    {
+      img: "",
+      name: "Microphones",
+      cost: 45,
+      quantity: 0,
+    },
+    {
+      img: "",
+      name: "Whiteboards",
+      cost: 80,
+      quantity: 0,
+    },
+    {
+      img: "",
+      name: "Signage",
+      cost: 80,
+      quantity: 0,
+    },
+
   ],
 
 
