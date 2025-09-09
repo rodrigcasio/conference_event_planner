b97086b3e92f712886a4cdcaa94c74726d3047eb
rodrigcasio <rod.casio@outlook.com>
2025-09-09 14:20:46 -0600

    Added functionality to display "avItems" data using a map() method and

--- src/ConferenceEvent.jsx
+++ src/ConferenceEvent.jsx
@@ -168,6 +168,13 @@ const ConferenceEvent = () => {
                                       <div className="img">
                                         <img src={item.img} alt={item.name} />
                                       </div>
+                                      <div className="text"> {item.name}</div>
+                                      <div> ${item.cost}</div>
+                                        <div className="addons_btn">
+                                          <button className="btn-warning" onClick={() => handleDecrementAvQuantity(index)}> &ndash; </button>
+                                          <span className="quantity-value">{item.quantity}</span>
+                                            <button className=" btn-success" onClick={() => handleIncrementAvQuantity(index)}> &#43; </button>
+                                        </div>
                                     </div>
                                   ))}    
                                 </div>
