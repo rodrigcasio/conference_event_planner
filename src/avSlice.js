7b9d85fca8699c57cae55b34652d75c8e8384db1
rodrigcasio <rod.casio@outlook.com>
2025-09-09 13:42:02 -0600

    Added correct path for images used for the value property of img

--- src/avSlice.js
+++ src/avSlice.js
@@ -1,35 +1,36 @@
 import { createSlice } from "@reduxjs/toolkit";
+import '../public/imgsFinalPractice';
 // 1. adding objects to provide data structure
 
 export const avSlice = createSlice({
   name: "av",
   initialState: [
     {
-      img: "",
+      img: "./imgsFinalPractice/projectors.jpg",
       name: "Projectors",
       cost: 200,
       quantity: 0,
     },
     {
-      img: "",
+      img: "./imgsFinalPractice/speakers.jpg",
       name: "Speaker",
       cost: 35,
       quantity: 0,
     },
     {
-      img: "",
+      img: "./imgsFinalPractice/microphone.jpg",
       name: "Microphones",
       cost: 45,
       quantity: 0,
     },
     {
-      img: "",
+      img: "./imgsFinalPractice/whiteboard.png",
       name: "Whiteboards",
       cost: 80,
       quantity: 0,
     },
     {
-      img: "",
+      img: "./imgsFinalPractice/signpost.jpg",
       name: "Signage",
       cost: 80,
       quantity: 0,
