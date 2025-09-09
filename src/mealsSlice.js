64e52705f59830e40382e90db6bff05457a8ea53
rodrigcasio <rod.casio@outlook.com>
2025-09-09 15:14:22 -0600

    Added logic for function reducer "toggleMealSelection"

--- src/mealsSlice.js
+++ src/mealsSlice.js
@@ -1,7 +1,7 @@
 // mealsSlice.js
 import { createSlice } from '@reduxjs/toolkit';
 // 1. including the meal items inside the initialState array
-
+// 2. adding the logic needed for function reducer toggleMealSelection
 export const mealsSlice = createSlice({
   name: 'meals',
   initialState: [
@@ -13,6 +13,7 @@ export const mealsSlice = createSlice({
   // reducer functions:
   reducers: {
     toggleMealSelection: (state, action) => {
+      state[action.payload].selected = ! state[action.payload].selected;      // uses [action.payload] to identify the item to update
     },
   },
 });
